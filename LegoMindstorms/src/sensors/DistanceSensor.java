0c86b2c5cd16fb5540a2ff0dd1e83f8402b134cf
david <ferrari599430@gmail.com>
2016-02-03 14:13:47 +0100

    Distance Sensor established

--- LegoMindstorms/src/sensors/DistanceSensor.java
+++ LegoMindstorms/src/sensors/DistanceSensor.java
@@ -1,7 +1,34 @@
 package sensors;
 
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
 public class DistanceSensor {
+	private EV3UltrasonicSensor distanceSensor;
+	private final String DISTANCE_STRING = "Distance";
+	
+	
 	public DistanceSensor(){
+		distanceSensor = new EV3UltrasonicSensor(SensorPort.S3);
+	}
+	
+	public float getDistance() {
+		SampleProvider distance= distanceSensor.getMode(DISTANCE_STRING);
+
+		float[] sample = new float[distance.sampleSize()];
+
+		distance.fetchSample(sample, 0);
+		distanceSensor.getDistanceMode();
+		
+		float result = 0;
+		
+		for(int i = 0; i < sample.length; i++) {
+			result += sample[i];
+		}
+		
+		return result / sample.length;
+		
 		
 	}
 }
--- LegoMindstorms/src/tasks/ChainBridge.java
+++ LegoMindstorms/src/tasks/ChainBridge.java
@@ -1,15 +1,20 @@
 package tasks;
 
+import sensors.DistanceSensor;
 import main.Main;
 
 public class ChainBridge extends Task {
+	private DistanceSensor sensor = null;
+	
 	public ChainBridge(Main main) {
 		super(main);
+		sensor = new DistanceSensor();
 	}
 
 	@Override
 	protected void specificExecute() {
-		// TODO Auto-generated method stub
+		//float distanceValue = sensor.getDistance(distance);
+		//LCD.drawString(String.valueOf(distanceValue), 0, 1);
 		
 	}
 }
