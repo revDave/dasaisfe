2b7b613bb22da875e3e1732e24161996819becfb
david <ferrari599430@gmail.com>
2016-02-03 14:26:36 +0100

    Actually, Bridge Traversing should work

--- LegoMindstorms/src/tasks/ChainBridge.java
+++ LegoMindstorms/src/tasks/ChainBridge.java
@@ -1,10 +1,12 @@
 package tasks;
 
 import sensors.DistanceSensor;
+import lejos.hardware.lcd.LCD;
 import main.Main;
 
 public class ChainBridge extends Task {
 	private DistanceSensor sensor = null;
+	private float BRIDGE_THRESHOLD = 0.01f;
 	
 	public ChainBridge(Main main) {
 		super(main);
@@ -13,8 +15,31 @@ public class ChainBridge extends Task {
 
 	@Override
 	protected void specificExecute() {
-		//float distanceValue = sensor.getDistance(distance);
-		//LCD.drawString(String.valueOf(distanceValue), 0, 1);
+		boolean leftPress = false;
+		boolean rightPress = false;
 		
+		if(leftPress) {
+			movement.rotateRight(20);
+		}
+		
+		if(rightPress) {
+			movement.rotateLeft(20);
+		}
+		
+		// TODO getting distance correctly
+		if(!leftPress && !rightPress) {
+			float distanceValue = 0.0f;//sensor.getDistance(distance)
+			
+			if(BRIDGE_THRESHOLD < distanceValue) {
+				escapeTakeDown();
+				LCD.drawString(String.valueOf(distanceValue), 0, 1);
+			}
+		}
+		
+	}
+	
+	// ultrasonic sensor is bowed to the right
+	private void escapeTakeDown() {
+		movement.rotateLeft(20);
 	}
 }
