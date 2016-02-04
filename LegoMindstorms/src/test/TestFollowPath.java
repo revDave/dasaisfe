a1ffb068fbaa7faee1ceee1115aea73fda6f9e9e
Felipe <fva.1985@gmail.com>
2016-02-04 14:56:11 +0100

    FollowPath searches for lost line

--- LegoMindstorms/src/test/TestFollowPath.java
+++ LegoMindstorms/src/test/TestFollowPath.java
@@ -1,11 +1,24 @@
 package test;
 
 import tasks.FollowPath;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.Key;
+import lejos.hardware.KeyListener;
 
 public class TestFollowPath{
 	public static void main(String[] args) {
 		FollowPath fp = new FollowPath(null);
 		
+		LocalEV3.get().getKey("Escape").addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(Key k) {
+            }
+
+            @Override
+            public void keyReleased(Key k) {
+                    System.exit(0);
+            }
+		});
 		fp.execute();
 	}
 }
