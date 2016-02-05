3297fb94b415b550cab3dc573c1177c9785a4309
Isabel Ehrenberger <isabelehrenberger@gitlab.ira.uka.de>
2016-02-05 11:49:21 +0100

    Zusätzliche Funktionen hinzu, Parameter angepasst in Labyrinth

--- LegoMindstorms/src/tasks/Labyrinth.java
+++ LegoMindstorms/src/tasks/Labyrinth.java
@@ -8,6 +8,8 @@ public class Labyrinth extends Task {
 
 	//TODO Test and fit
 	private final float LAB_THRESHOLD = 0.1f;
+	private final float AWAY_THRESHOLD = 0.20f;
+	private final float NEAR_THRESHOLD = 0.02f;
 	
 	public Labyrinth(Main main) {
 		super(main);
@@ -15,14 +17,14 @@ public class Labyrinth extends Task {
 
 	@Override
 	protected void specificExecute() {
-		movement.setSpeeds(15, 20);
+		movement.setSpeeds(25, 20);
 		//checks if distanceSensor is up or down
 		movement.unbowSensor();
 
 		// if robot touches the wall in front oh it 
 		if (tactileSensor.frontIsPressed()) {
-			movement.travel(-2);
-			movement.rotateRight(80);
+			movement.travel(-3);
+			movement.rotateRight(90);
 		}
 		// check if there is a wall on the left side by using DistanceSensor
 		// if there is a wall, drive straight forward, if not, rotate about 90 degrees 
@@ -32,10 +34,17 @@ public class Labyrinth extends Task {
 			LCD.drawString(String.valueOf(distanceValue), 0, 1);
 			
 			if(LAB_THRESHOLD < distanceValue) {
-				movement.travel(5);
+				movement.travel(15);
 				movement.rotateLeft(90);
 				movement.travel(5);
-				
+			}
+			// Robot is too near to the wall
+			else if (NEAR_THRESHOLD > distanceValue){
+				movement.rotateRight(5);
+			}
+			// Robot is too far away from the wall
+			else if (AWAY_THRESHOLD < distanceValue) {
+				movement.rotateLeft(5);
 			// drive through the labyrinth
 			} else {
 				movement.driveForward();
