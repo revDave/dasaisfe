4ce61292ade368f1bed62223bf90a3ce4cb1d04c
Sandro Koch <layornos@gmail.com>
2016-02-05 01:08:08 +0100

    created d controller class

---
+++ LegoMindstorms/src/control/modules/D_Control.java
@@ -0,0 +1,23 @@
+package control.modules;
+
+public class D_Control {
+	private double lastError = 0.;
+	private double kd;
+	private double derivative = 0.;
+
+	public D_Control(double kd){
+		this.kd = kd;
+	}
+	
+	public double calcOutput(double error){
+		derivative = lastError - error;
+		double output = kd * derivative;
+		lastError = error;
+		return output;
+	}
+	
+	public void reset(){
+		lastError = 0.;
+		derivative = 0.;
+	}
+}
