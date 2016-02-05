90db882317912453fb293468a94f80a606174396
Sandro Koch <layornos@gmail.com>
2016-02-05 01:08:39 +0100

    created i controller class

---
+++ LegoMindstorms/src/control/modules/I_Control.java
@@ -0,0 +1,21 @@
+package control.modules;
+
+public class I_Control {
+	private double constant = 1.;
+	private double ki;
+	private double integral = 0.;
+	
+	public I_Control(double ki, double constant){
+		this.constant = constant;
+		this.ki = ki;
+	}
+	
+	public double calcOutput(double error){
+		integral = constant * integral + error;
+		return integral * ki;
+	}
+	
+	public void reset(){
+		integral = 0;
+	}
+}
