651b214fd544f19542b99c5f87e2534887f5d700
david <ferrari599430@gmail.com>
2016-02-05 16:01:27 +0100

    Introduced Regulated Control

--- LegoMindstorms/src/tasks/FollowPath.java
+++ LegoMindstorms/src/tasks/FollowPath.java
@@ -1,56 +1,25 @@
 package tasks;
 
+import sensors.DistanceSensor;
 import main.Main;
 import lejos.hardware.lcd.LCD;
 import control.modules.PID_Control;
 import lejos.hardware.Button;
 import lejos.utility.Stopwatch;
 
-public class FollowPath extends Task {
-	private double OFFSET = (double) 0.5;
-	private float ERROR_TRESHOLD = (float) 0.15;
-	private float KC = 300;
-	private float PC = 500;
-	private float DT = 15;
-	private double P_FACTOR = 0.6;
-	private double I_FACTOR = 2.;
-	private double I_CONSTANT = 2 / 3;
-	private double D_FACTOR = 8;
-	private double error = 0;
+public class FollowPath extends RegulatedTask {
 	private double turn = 0;
 	private boolean iAmLost = true;
 	private int maxLostTime = 300;
-	private float wheelSpeed = (float) 4;
 	private Stopwatch finderWatch;
 	private Stopwatch lostWatch;
-
-	private PID_Control pid;
 
 	public FollowPath(Main main) {
 		super(main);
 
-		/**
-		 * Example of the pid controller define the kc, pc and dt values,
-		 * preferably as private attributes run pid.init() with the following
-		 * arguments, same as above
-		 * 
-		 * @param p_factor
-		 * @param i_factor
-		 * @param i_CONSTANT
-		 * @param d_factor
-		 * 
-		 */
-		pid = new PID_Control(KC, PC, DT);
-		pid.init(P_FACTOR, I_FACTOR, I_CONSTANT, D_FACTOR);
-		pid.setOffset(OFFSET);
-
-		movement.setSpeeds(wheelSpeed, 120);
 		finderWatch = new Stopwatch();
 		lostWatch = new Stopwatch();
-
-		LCD.drawString("Press button to start", 0, 1);
-		Button.waitForAnyPress();
-		LCD.clear();
+		
 		findline(false);
 	}
 
@@ -59,9 +28,8 @@ public class FollowPath extends Task {
 		findline(true);
 
 		// Get read from sensor
-		double red = colorSensor.getRedSensorValue();
-		error = red - OFFSET;
-		if (red < ERROR_TRESHOLD) {
+		double sensorValue = getSensorValue();
+		if (sensorValue < getLostThreshold()) {
 			if (lostWatch.elapsed() > maxLostTime) {
 				iAmLost = true;
 
@@ -73,14 +41,7 @@ public class FollowPath extends Task {
 			lostWatch.reset();
 		}
 
-		// calc pid output with the given error
-		turn = pid.calcOutput(error);
-
-		turn = turn < -200 ? -200 : turn;
-		turn = turn > 200 ? 200 : turn;
-
-		movement.steer(turn, false);
-		finderWatch.reset();
+		super.specificExecute();
 	}
 
 	protected void findline(boolean reverse) {
@@ -91,8 +52,8 @@ public class FollowPath extends Task {
 				finderWatch.reset();
 				movement.steer(turn, reverse);
 				while (iAmLost && finderWatch.elapsed() < i) {
-					float red = colorSensor.getRedSensorValue();
-					if (red >= ERROR_TRESHOLD) {
+					float sensorValue = getSensorValue();
+					if (sensorValue >= getLostThreshold()) {
 						iAmLost = false;
 						lostWatch.reset();
 					}
@@ -102,5 +63,25 @@ public class FollowPath extends Task {
 				}
 			}
 		}
+	}
+	
+	@Override
+	protected float getSensorValue() {
+		return colorSensor.getRedSensorValue();
+	}
+
+	@Override
+	protected float getOffset() {
+		return 0.5f;
+	}
+
+	@Override
+	protected float getKC() {
+		return 300;
+	}
+
+	@Override
+	protected float getLostThreshold() {
+		return 0.15f;
 	}
 }
