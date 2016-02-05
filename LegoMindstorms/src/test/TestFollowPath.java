1d63e0c8bcfff6f526a2f4b624b4a052478cac43
Felipe <fva.1985@gmail.com>
2016-02-05 13:51:02 +0100

    Created TestBase class to avoid programs without escape

--- LegoMindstorms/src/test/TestFollowPath.java
+++ LegoMindstorms/src/test/TestFollowPath.java
@@ -5,20 +5,10 @@ import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.Key;
 import lejos.hardware.KeyListener;
 
-public class TestFollowPath{
+public class TestFollowPath extends TestBase{
 	public static void main(String[] args) {
 		FollowPath fp = new FollowPath(null);
 		
-		LocalEV3.get().getKey("Escape").addKeyListener(new KeyListener() {
-            @Override
-            public void keyPressed(Key k) {
-            }
-
-            @Override
-            public void keyReleased(Key k) {
-                    System.exit(0);
-            }
-		});
 		fp.execute();
 	}
 }
