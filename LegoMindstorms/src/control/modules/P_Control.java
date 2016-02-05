00ba1fb57d175b57d4d2ef0228397f8301505cc0
Sandro Koch <layornos@gmail.com>
2016-02-05 01:07:29 +0100

    created p controler class

---
+++ LegoMindstorms/src/control/modules/P_Control.java
@@ -0,0 +1,23 @@
+package control.modules;
+
+public class P_Control {
+	private double kp;
+	
+	
+	public P_Control(double kp){
+		this.kp = kp;
+	}
+	
+	public double calcOutput(double error){
+		return kp * error; 
+	}
+
+	public double getKp() {
+		return kp;
+	}
+	
+	
+	
+	
+	
+}
