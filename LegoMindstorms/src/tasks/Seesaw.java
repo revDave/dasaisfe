023e9bae79b19d9ddb3bc48f203bb5c4cfc346dd
Isabel Ehrenberger <isabelehrenberger@gitlab.ira.uka.de>
2016-02-08 16:44:52 +0100

    Ausversehen im falschen branch gearbeitet, seesaw noch angepasst

--- LegoMindstorms/src/tasks/Seesaw.java
+++ LegoMindstorms/src/tasks/Seesaw.java
@@ -5,42 +5,42 @@ import lejos.hardware.lcd.LCD;
 import lejos.utility.Delay;
 import main.Main;
 
-public class Seesaw extends RegulatedTask {
+public class Seesaw extends FollowPath{
 	
 	public Seesaw(Main main) {
 		super(main);
 	}
 	
-	@Override
-	public void specificExecute() {
-		super.specificExecute();
-	}
-
-	@Override
-	protected float getSensorValue() {
-		float result = DistanceSensor.getInstance().getDistance();
-		
-		if(result > 0.6) {
-			result = 0.6f;
-		}
-		
-		return result;
-	}
-
-	@Override
-	protected float getOffset() {
-		return 0.08f;
-	}
-
-	@Override
-	protected float getKC() {
-		return 650;
-	}
-
-	@Override
-	protected float getLostThreshold() {
-		return 0;
-	}
+//	@Override
+//	public void specificExecute() {
+//		super.specificExecute();
+//	}
+//
+//	@Override
+//	protected float getSensorValue() {
+//		float result = DistanceSensor.getInstance().getDistance();
+//		
+//		if(result > 0.6) {
+//			result = 0.6f;
+//		}
+//		
+//		return result;
+//	}
+//
+//	@Override
+//	protected float getOffset() {
+//		return 0.08f;
+//	}
+//
+//	@Override
+//	protected float getKC() {
+//		return 650;
+//	}
+//
+//	@Override
+//	protected float getLostThreshold() {
+//		return 0;
+//	}
 
 }
 
