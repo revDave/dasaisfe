c8f0c8ff47d86c365df3c5eef1d9a70839e21866
Sandro Koch <layornos@gmail.com>
2016-02-09 11:21:39 +0100

    ele

--- LegoMindstorms/src/tasks/Elevator.java
+++ LegoMindstorms/src/tasks/Elevator.java
@@ -1,13 +1,12 @@
 package tasks;
 
-import main.Main;
-
 public class Elevator extends Task {
 	
 	@Override
 	protected TaskState specificExecute() {
 		// TODO Auto-generated method stub
 		return TaskState.CONTINUE;
+		
 	}
 
 }
