9b5dae12afa12742d21100c80c0ff86668060b5e
Sandro Koch <layornos@gmail.com>
2016-02-09 10:40:18 +0100

    switch completed

--- LegoMindstorms/src/sensors/BarcodeScanner.java
+++ LegoMindstorms/src/sensors/BarcodeScanner.java
@@ -2,6 +2,7 @@ package sensors;
 
 import lejos.hardware.lcd.LCD;
 import lejos.utility.Stopwatch;
+import main.ParkourStage;
 
 public class BarcodeScanner {
 	private ColorSensor cs;
@@ -11,17 +12,17 @@ public class BarcodeScanner {
 
 	public BarcodeScanner(Movement m) {
 		mov = m;
-		cs = new ColorSensor();
+		cs = ColorSensor.getInstance();
 		changed = false;
 		lineCount = 0;
 	}
 
-	public int read() {
+	public ParkourStage read() {
 		mov.setSpeeds(1, 1337);
 		mov.driveForward(); 
 		Stopwatch timer = new Stopwatch(); 
 		timer.reset(); 
-		while (timer.elapsed() < 10000) { 
+		while (timer.elapsed() < 3500) { 
 			float color = cs.getRedSensorValue(); 
 			LCD.drawString("Timer: " + timer.elapsed(), 0, 4);
 			LCD.drawString("color: " + color, 0, 6);
@@ -37,12 +38,32 @@ public class BarcodeScanner {
 			}
 		}
 		mov.stop();
-		return lineCount;
+		
+		return mapLineCountToStage(lineCount);
 	}
 
 	public void reset() {
 		lineCount = 0;
 		changed = false;
+	}
+	
+	private ParkourStage mapLineCountToStage(int lineCount){
+		switch (lineCount) {
+		case 1:
+			return ParkourStage.IDLE;
+		case 2:
+			return ParkourStage.FOLLOWPATH;
+		case 3:
+			return ParkourStage.BRIDGE;
+		case 4:
+			return ParkourStage.SEESAW;
+		case 5:
+			return ParkourStage.CHAINBRIDGE;
+		case 6:
+			return ParkourStage.BOSS;
+		default:
+			return ParkourStage.IDLE;
+		}
 	}
 
 }
