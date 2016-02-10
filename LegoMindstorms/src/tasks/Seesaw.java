f5dffc66474485b63479b3087192f3248ab23324
Sandro Koch <layornos@gmail.com>
2016-02-10 13:35:07 +0100

    Seesaw constr.

--- LegoMindstorms/src/tasks/Seesaw.java
+++ LegoMindstorms/src/tasks/Seesaw.java
@@ -3,11 +3,14 @@ package tasks;
 import lejos.utility.Delay;
 import lejos.utility.Stopwatch;
 import main.Main;
+import sensors.Movement;
 
 public class Seesaw extends FollowPath{
 	public Seesaw(){
 		maxWheelSpeed = 4.f;
 		currentWheelSpeed = maxWheelSpeed;
+		Movement.getInstance().travel(15);
+		Movement.getInstance().rotateLeft(15);
 	}
 
 
